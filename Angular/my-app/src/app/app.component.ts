eca9285540381baccd000fec74bfbe5503912676
nikitaSufranovichJS <58137290+nikitaSufranovichJS@users.noreply.github.com>
2019-12-05 20:14:20 +0300

    refactor naming

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -8,7 +8,7 @@ export class User {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
   
 })
 export class AppComponent {
